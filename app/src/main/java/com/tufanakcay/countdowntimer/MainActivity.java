46ec8a18b1c37f817fedeef72927caf1b300131c
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-13 20:36:53 +0300

    Update MainActivity.java

--- app/src/main/java/com/tufanakcay/countdowntimer/MainActivity.java
+++ app/src/main/java/com/tufanakcay/countdowntimer/MainActivity.java
@@ -3,12 +3,18 @@ package com.tufanakcay.countdowntimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        
     }
 }
